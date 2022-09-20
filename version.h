432c28f05a69349c9eebd1006b357bff3980652e
Lucifer <x-lucifer@qq.com>
2022-09-21 01:23:50 +0800

    init

---
+++ version.h
@@ -0,0 +1,7 @@
+#define NSSM_VERSION _T("3.0")
+#define NSSM_VERSIONINFO 3,0,0,0
+#define NSSM_DATE _T("2022/09/21")
+#define NSSM_FILEFLAGS VS_FF_PRERELEASE
+#define NSSM_COPYRIGHT _T("Copyright (C) Lucifer. All rights reserved.")
+#define NSSM_COMMENTS _T("https://www.nssm.cc")
+#define NSSM_COMPANYNAME _T("X Lucifer")
